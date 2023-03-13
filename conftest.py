c3990f96cda31ea2cda590e6507d986c98766bfe
Roman Chornyi <romchablack2@gmail.com>
2023-03-13 17:32:22 +0000

    Added conftest.py

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from modules.api.clients.github import GitHub
+from modules.common.database import Database
+
+
+class User:
+
+    def __init__(self) -> None:
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = 'Roman'
+        self.second_name = 'Chornyi'
+
+    def remove(self):
+        self.name = ''
+        self.second_name = ''
+
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user
+    user.remove()
+
+
+@pytest.fixture
+def github_api():
+    api = GitHub()
+    yield api
+
+
+@pytest.fixture
+def create_database():
+    db = Database()
+    yield db
