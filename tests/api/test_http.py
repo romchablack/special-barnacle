f04e56c06ffbb10aca7901f946fe4847054073b1
Roman Chornyi <romchablack2@gmail.com>
2023-03-13 17:40:39 +0000

    Add tests/api

---
+++ tests/api/test_http.py
@@ -0,0 +1,23 @@
+import pytest
+import requests
+
+
+@pytest.mark.http
+def test_first_request():
+    r = requests.get('https://api.github.com/zen')
+    print(r.text)
+
+
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('https://api.github.com/users/defunkt')
+
+    assert r.json()['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert r.headers['Server'] == 'GitHub.com'
+
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/sergii_butenko')
+    assert r.status_code == 404
